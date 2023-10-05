ffbc6cd9573b3471c37c2d107de9337d9c3e7830
Amyat103 <zhoudavid7312@gmail.com>
2023-10-04 22:51:47 -0400

    added oauth and request token from API based on twitterAPI documentations. still setting up bot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import key
 import tweepy
+from requests_oauthlib import OAuth1Session
 
 client = tweepy.Client(key.BEARER_TOKEN, key.API_KEY, key.API_KEY_SECRET, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET)
 oauth2_user_handler = tweepy.OAuth2UserHandler(
@@ -9,8 +10,23 @@ oauth2_user_handler = tweepy.OAuth2UserHandler(
     scope="",
     client_secret=key.CLIENT_SECRET
 )
+
+id = 27851463
+
+auth = tweepy.OAuth1UserHandler(key.API_KEY, key.API_KEY_SECRET, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 print(oauth2_user_handler.get_authorization_url())
 
-client.create_tweet(text="Hi")
+oauth = OAuth1Session(key.API_KEY, client_secret=key.API_KEY_SECRET)
 
+request_token_url = "https://api.twitter.com/oauth/request_token"
+response = oauth.fetch_request_token(request_token_url)
+
+resource_owner_key = response.get("oauth_token")
+resource_owner_secret = response.get("oauth_token_secret")
+print("OAuth Token" % resource_owner_key)
+
+
+#tweet text
+# client.create_tweet(text="Hi")
 
