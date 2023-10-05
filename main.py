f4923828dd693a4cde35e1829690e40f0424317d
Amyat103 <zhoudavid7312@gmail.com>
2023-10-04 23:04:46 -0400

    removed like/unlike, not accessable on free tier

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import key
 import tweepy
 from requests_oauthlib import OAuth1Session
 
-client = tweepy.Client(key.BEARER_TOKEN, key.API_KEY, key.API_KEY_SECRET, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET)
+#consumer key stand for API_key
+client = tweepy.Client(
+    key.BEARER_TOKEN,
+    key.API_KEY,
+    key.API_KEY_SECRET,
+    key.ACCESS_TOKEN,
+    key.ACCESS_TOKEN_SECRET)
 oauth2_user_handler = tweepy.OAuth2UserHandler(
     client_id=key.API_KEY,
     redirect_uri="",
@@ -11,19 +17,19 @@ oauth2_user_handler = tweepy.OAuth2UserHandler(
     client_secret=key.CLIENT_SECRET
 )
 
-
 auth = tweepy.OAuth1UserHandler(key.API_KEY, key.API_KEY_SECRET, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 print(oauth2_user_handler.get_authorization_url())
+
 
 tweet_id_test = 1709758382083817636
 
 #tweet text
 # client.create_tweet(text="Hi")
 
-# like/unlike tweet
-client.like(tweet_id_test)
-client.unlike(temp_post_id)
+# like/unlike tweet(CANNOT USE ON FREE TIER)
+# client.like(tweet_id=tweet_id_test)
+# # client.unlike(temp_post_id)
 
 
 # TWITTER API GUIDE(MIGHT NOT NEED SINCE USING TWEEPY)
